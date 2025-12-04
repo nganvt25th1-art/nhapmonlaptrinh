efca263c6e2aa4cc412a5f7af9592dcf150e97e0
Buoi 5 <Administrator@PMT2004>
2025-12-04 13:20:41 +0700

    bai tap buoi 5

--- NhapMonLapTrinh/Buoi5/Buoi5.cpp
+++ NhapMonLapTrinh/Buoi5/Buoi5.cpp
@@ -22,10 +22,41 @@ void cau2() {
         cout << "khac dau";
 
 }
+void cau3() {
+    int a, b;
+    cout << "Nhap a, b:";
+    cin >> a >> b;
+    if (a == 0) {
+        if (b == 0)
+            cout << "Phuong trinh vo so nghiem:" << endl;
+        else
+            cout << "Phuong trinh vo nghiem: " << endl;
+    }
+    else {
+        cout << "Nghiem la:" << (-b * 1.0 / a) << endl;
+    }
+}
+void cau4() {
+    int thang;
+    cout << "Nhap thang:";
+    cin >> thang;
+    if (thang <= 3) {
+        cout << "Quy 1";
+    }
+    else if (thang <= 6) {
+        cout << "Quy 2";
+    }
+    else if (thang <= 9) {
+        cout << "Quy 3";
+    }
+    else {
+        cout << "Quy 4";
+    }
+}
 
 int main()
 {
-    cau2();
+    cau4();
 }
 
 
