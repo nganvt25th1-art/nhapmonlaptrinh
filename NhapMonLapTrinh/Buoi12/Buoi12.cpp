04739e48b0973e3cd7bc72b389e3c9e02617b5d9
Buoi 7 <Administrator@PMT2004>
2025-12-18 16:00:09 +0700

    bai tap buoi 12

--- NhapMonLapTrinh/Buoi12/Buoi12.cpp
+++ NhapMonLapTrinh/Buoi12/Buoi12.cpp
@@ -5,6 +5,11 @@ int main()
 {
 	int a[MAX], n = 0;
 	input(a, n);
-	output(a, n);
+	//output(a, n);
+	//giatrichan(a, n);
+	//vitriam(a, n);
+	//giatrilonnhat(a, n);
+	//vitrinhonhat(a, n);
+	nguyentonhohon100(a, n);
 }
 
--- NhapMonLapTrinh/Buoi12/Les.cpp
+++ NhapMonLapTrinh/Buoi12/Les.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "SoNguyen.h"
 using namespace std;
 
 void input(int b[], int& n) {
@@ -15,6 +16,53 @@ void output(int b[], int n) {
 	while (i < n) {
 		cout << b[i] << "   ";
 		i++;
+	}
+	cout << endl;
+}
+void giatrichan(int b[], int n) {
+	cout << "Danh sach gia tri chan: " << endl;
+	int i = 0;
+	while (i < n) {
+		if (b[i] % 2 == 0)
+			cout << b[i] << "   ";
+		i++;
+	}
+	cout << endl;
+}
+void vitriam(int b[], int n) {
+	cout << "Danh sach vi tri am:" << endl;
+	int i = 0;
+	while (i < n) {
+		if (b[i] < 0)
+			cout << i + 1 << "   ";
+		i++;
+	}
+	cout << endl;
+}
+void giatrilonnhat(int b[], int n) {
+	int max = b[0];
+	for (int i = 1; i < n; i++) {
+		if (b[i] > max)
+			max = b[i];
+	}
+	cout << "Gia tri lon nhat: " << max << endl;
+}
+void vitrinhonhat(int b[], int n) {
+	int min = b[0], vt = 0;
+	for (int i = 1; i < n; i++) {
+		if (b[i] > min) {
+			min = b[i];
+			vt = i;
+		}
+	}
+	cout << "Vi tri co gia tri nho nhat: " << min << endl;
+}
+void nguyentonhohon100(int b[], int n) {
+	cout << "Nguyen to nho hon 100: " << endl;
+	for (int i = 0; i < n; i++) {
+		if (b[i] < 100 && NguyenTo(b[i]) == true) {
+			cout << b[i] << "   ";
+		}
 	}
 	cout << endl;
 }
